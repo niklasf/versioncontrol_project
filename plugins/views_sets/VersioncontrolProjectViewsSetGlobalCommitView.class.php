9ffb3e880107986649c169e4bbd2297e30fd5900
Sam Boyer <drupal@samboyer.org>
2011-01-03 12:32:28 +0000

    Introduce vc_project-owned views sets for the global commit log and per-user commit log.
    Per-repository commit log should hopefully not be necessary, since it's
    redundant to list the project name in an already single-repo listing.

---
+++ plugins/views_sets/VersioncontrolProjectViewsSetGlobalCommitView.class.php
@@ -0,0 +1,7 @@
+<?php
+
+class VersioncontrolProjectViewsSetGlobalCommitView extends VersioncontrolViewsSetGlobalBase {
+  protected $setName = 'project_global_commit_view';
+
+  protected $baseView = 'vc_project_global_commits';
+}
---
+++ plugins/views_sets/VersioncontrolProjectViewsSetUserCommitView.class.php
@@ -0,0 +1,7 @@
+<?php
+
+class VersioncontrolProjectViewsSetUserCommitView extends VersioncontrolViewsSetGlobalBase {
+  protected $setName = 'project_user_commit_view';
+
+  protected $baseView = 'vc_project_user_commits';
+}
