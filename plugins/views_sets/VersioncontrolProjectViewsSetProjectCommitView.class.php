a837091c2993b5197789e2c9ca061a090cecf03a
Michael Prasuhn <mike@mikeyp.net>
2011-01-17 04:59:08 +0000

    #1025334 by mikey_p: Provide per-project view, link from project page, and views_set for per-project commit views, update project_global commit view and project per user view

---
+++ plugins/views_sets/VersioncontrolProjectViewsSetProjectCommitView.class.php
@@ -0,0 +1,7 @@
+<?php
+
+class VersioncontrolProjectViewsSetProjectCommitView extends VersioncontrolViewsSetGlobalBase {
+  protected $setName = 'project_commit_view';
+
+  protected $baseView = 'vc_project_commit_view';
+}
