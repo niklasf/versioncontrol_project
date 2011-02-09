4d42c297df03e9b743b136a1cffaaf0780904b28
Marco Villegas <marvil07@gmail.com>
2011-02-09 14:50:20 -0500

    Issue #1056568 by marvil07, sdboyer: Provide rss on commitlog views.
    - Update vc_project_commit_view, vc_project_global_commits,
      vc_project_user_commits, vc_git_project_commit_view,
      vc_git_project_global_commits, and vc_git_project_user_commits views.
    - Use the project repository to define the view to show on
      node/nid/commits.

--- plugins/views_sets/VersioncontrolProjectViewsSetProjectCommitView.class.php
+++ plugins/views_sets/VersioncontrolProjectViewsSetProjectCommitView.class.php
@@ -8,4 +8,13 @@ class VersioncontrolProjectViewsSetProjectCommitView extends VersioncontrolViews
   protected $defaultViews = array(
     'git' => 'vc_git_project_commit_view',
   );
+
+  /**
+   * This view is shown assuming a nid argument, so we can use
+   * getViewNameByBackend() to define the view on a multi-backends case,
+   * since one project can only have one repository(one backend) associated.
+   */
+  public function getViewNameByEntity(VersioncontrolEntityInterface $entity) {
+    return $this->getViewNameByBackend($entity->getBackend());
+  }
 }
